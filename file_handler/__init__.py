1dd5f89b49393f0cf75e6a591b021cfee17b71b3
tian <573367949@qq.com>
2016-04-17 13:18:23 +0800

    Finished data_dict.

--- file_handler/__init__.py
+++ file_handler/__init__.py
@@ -1,3 +1,4 @@
 # coding=utf-8
 # Created by Tian Yuanhao on 2016/4/9.
 
+import data_dict
--- file_handler/data_dict.py
+++ file_handler/data_dict.py
@@ -1,23 +1,54 @@
 # coding=utf-8
 # Created by Tian Yuanhao on 2016/4/12.
+from frontend.nodes import AttrType
 
 
 class DataDict:
     def __init__(self, file_path):
-        self.file_path = file_path
         self.dict = {}
+        self.file_path = file_path
         self.load_data()
 
-    def table_info(self, table_name):
-        pass
-
     def tables_name(self):
-        pass
+        return self.dict.keys()
 
     def load_data(self):
-        pass
+        f = open(self.file_path, "r")
+        table_name = "default"
+        while True:
+            line = f.readline()
+            if not line: break
+            if len(line) == "\n": continue
+
+            if line[0] == '[':
+                table_name = line[1:-2]
+                print table_name
+            elif len(line) > 1:
+                items = line[:-1].split()
+                if not self.dict.has_key(table_name):
+                    self.dict[table_name] = [AttrType(*items)]
+                else:
+                    self.dict[table_name] += [AttrType(*items)]
+        f.close()
 
     def write_back(self):
-        pass
+        text = ""
+        for key, val in self.dict.items():
+            text += "[" + key + "]\n"
+            for type in val: text += str(type) + "\n"
+            text += "\n"
+        f = open(self.file_path, "w")
+        f.write(text)
+        f.close()
 
+
+
+data = DataDict("data.txt")
+print data.tables_name()
+
+# data.dict = {
+#     "A" : [AttrType("age", "INT", 0), AttrType("name", "CHAR", 10)],
+#     "B" : [AttrType("age", "INT", 0), AttrType("name", "CHAR", 10), AttrType("age", "PK", 0)]
+# }
+# data.write_back()
 
